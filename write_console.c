2dad8abea7a072a54949857241b55ecfd0233f3d
cheacase <andriychea@gmail.com>
2017-02-05 22:55:09 +0200

    Add write funcion

--- write_console.c
+++ write_console.c
@@ -2,3 +2,20 @@
 // Created by Andriy Yatsynyak on 2/5/17.
 //
 
+#include "libftprintf.h"
+
+//void    switsh_write(void * value, t_fmt fmt)
+//{
+//    if (ft_strchr("oOxX", fmt.specifier))
+//
+//}
+
+
+void    write_decimal(void  *value, t_fmt fmt)
+{
+    char *num;
+    char t = fmt.specifier;
+
+    num = ft_itoa((int)value);
+    ft_putstr(num);
+}
